120cf8a417ea2d8ff5cd10142473c54f3f94a5ab
ELLLPE <elias.arvidsson08@gmail.com>
2026-09-04 14:11:57 +0200

    init

---
+++ www/public/phpTest/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head lang="sv">
+</head>
+<body>
+<h1>PHP-test</h1>
+<?php
+   $tal1 = 5;
+   $tal2 = 3;
+   $sum = $tal1 + $tal2;
+  
+   echo("<p>" . $tal1 . " + " . $tal2 . " = " . $sum . "<p>");
+?>
+</body>
+</html> 
